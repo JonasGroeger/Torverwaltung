f2c76864f6063ec4674b5ed8e57eb193d0e52c92
Jonas Groeger <jonas.groeger@googlemail.com>
2012-09-01 03:08:30 +0200

    Enabled admin. Weird names though.

---
+++ Torverwaltung/admin.py
@@ -0,0 +1,8 @@
+from Torverwaltung.models import Spieler, Mannschaft, Spieltyp, Spiel, Einsatz
+from django.contrib import admin
+
+admin.site.register(Spieler)
+admin.site.register(Mannschaft)
+admin.site.register(Spieltyp)
+admin.site.register(Spiel)
+admin.site.register(Einsatz)
